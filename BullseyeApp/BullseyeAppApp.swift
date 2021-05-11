f87fba49f7184b41a98e63fde9e7bb85b9769789
Otabek0780 <mrdunacdtm@gmail.com>
2021-05-12 01:55:23 +0900

    Initial Commit

---
+++ BullseyeApp/BullseyeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeAppApp.swift
+//  BullseyeApp
+//
+//  Created by kim jong moon on 12/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
